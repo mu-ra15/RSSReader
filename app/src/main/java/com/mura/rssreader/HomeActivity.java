499ef3a993f53efe8448dad458027b771569dc7f
mura <s15.f.st@gmail.com>
2016-03-24 11:43:17 +0900

    change snackbar time

--- app/src/main/java/com/mura/rssreader/HomeActivity.java
+++ app/src/main/java/com/mura/rssreader/HomeActivity.java
@@ -31,7 +31,7 @@ public class HomeActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
             }
         });
