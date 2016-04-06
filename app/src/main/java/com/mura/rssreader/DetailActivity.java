23dbeb0d2c2e5c46efbf171c698f1661d48157b4
kiyoshiro <s15.f.st@gmail.com>
2016-04-06 14:59:30 +0900

    add combine bitmap save function

--- app/src/main/java/com/mura/rssreader/DetailActivity.java
+++ app/src/main/java/com/mura/rssreader/DetailActivity.java
@@ -1,8 +1,15 @@
 package com.mura.rssreader;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,9 +24,13 @@ import android.widget.ImageView;
 import com.sangcomz.fishbun.FishBun;
 import com.sangcomz.fishbun.define.Define;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements MainController.Callbacks {
     private static String TAG = "DetailActivity";
 
     ArrayList<String> path = new ArrayList<>();
@@ -28,6 +39,9 @@ public class DetailActivity extends AppCompatActivity {
     LinearLayoutManager linearLayoutManager;
     GallaryAdapter mainAdapter;
     MainController mainController;
+    ImageView mDragView;
+    Bitmap mBgImg;
+    Bitmap mFgImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +62,25 @@ public class DetailActivity extends AppCompatActivity {
         imgMain = (ImageView) findViewById(R.id.img_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        mainController = new MainController(this, imgMain);
+        mainController = new MainController(this, imgMain, this);
         mainAdapter = new GallaryAdapter(this, mainController, path);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(mainAdapter);
 
+        // ドラッグ対象Viewとイベント処理クラスを紐付ける
+        mDragView = (ImageView) findViewById(R.id.logo_img);
+        DragViewListener listener = new DragViewListener(mDragView);
+        mDragView.setOnTouchListener(listener);
 
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFgImg = ((BitmapDrawable)mDragView.getDrawable()).getBitmap();
+                mainController.getImgMain();
+            }
+        });
 
         FishBun.with(DetailActivity.this)
                 .setPickerCount(20)
@@ -65,6 +92,13 @@ public class DetailActivity extends AppCompatActivity {
                 .setButtonInAlbumActiviy(true)
                 .setCamera(true)
                 .startAlbum();
+    }
+
+    // 画像取得コールバック
+    @Override
+    public void callbackMethod(Bitmap bgImg) {
+        Bitmap combineImages = combineImages(bgImg, mFgImg);
+        saveBitamap(combineImages);
     }
 
     protected void onActivityResult(int requestCode, int resultCode,
@@ -95,5 +129,46 @@ public class DetailActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public Bitmap combineImages(Bitmap bgd, Bitmap fg) {
+        Bitmap cs;
+
+        int width = bgd.getWidth() > fg.getWidth()
+                ? bgd.getWidth() : fg.getWidth();
+        int height = bgd.getHeight() > fg.getHeight()
+                ? bgd.getHeight() : fg.getHeight();
+
+        cs = Bitmap.createBitmap( width, height,
+                Bitmap.Config.ARGB_8888 );
+
+        Canvas comboImage = new Canvas( cs );
+        comboImage.drawBitmap(bgd, new Matrix(), null );
+        comboImage.drawBitmap( fg, new Matrix(), null );
+
+        return cs;
+    }
+
+    private void saveBitamap(Bitmap bitmap){
+        try {
+            // sdcardフォルダを指定
+            File root = Environment.getExternalStorageDirectory();
+
+            // 日付でファイル名を作成　
+            Date mDate = new Date();
+            SimpleDateFormat fileName = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
+            // 保存処理開始
+            FileOutputStream fos = null;
+            fos = new FileOutputStream(new File(root, fileName.format(mDate) + ".jpg"));
+
+            // jpegで保存
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+
+            // 保存処理終了
+            fos.close();
+        } catch (Exception e) {
+            Log.e("Error", "" + e.toString());
+        }
     }
 }
--- app/src/main/java/com/mura/rssreader/MainController.java
+++ app/src/main/java/com/mura/rssreader/MainController.java
@@ -1,22 +1,55 @@
 package com.mura.rssreader;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+
+import java.util.concurrent.ExecutionException;
+
 /**
  * Created by mura on 16/04/04.
  */
 public class MainController {
     Context context;
     ImageView imgMain;
+    String mPath;
+    private Callbacks getCallback;
 
-    MainController(Context context, ImageView imgMain) {
+    public interface Callbacks {
+        public void callbackMethod(Bitmap bitmap);
+    }
+
+    MainController(Context context, ImageView imgMain, Callbacks callback) {
         this.context = context;
         this.imgMain = imgMain;
+        this.getCallback = callback;
     }
 
     public void setImgMain(String path) {
         Glide.with(context).load(path).fitCenter().into(imgMain);
+        mPath = path;
+
+    }
+
+    public void getImgMain(){
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    Bitmap Bitmap = Glide.with(context).load(mPath).asBitmap().into(-1, -1).get();
+                    getCallback.callbackMethod(Bitmap);
+                    Log.d("TEST", "aaaa");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
     }
 }
